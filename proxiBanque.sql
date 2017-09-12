249fc2de2c63c3ca655e4412cce3b0f75f073196
Xavier <xpersil@gmail.com>
2017-09-12 16:21:38 +0200

    Fin table SQL

--- proxiBanque.sql
+++ proxiBanque.sql
@@ -23,10 +23,40 @@ adresse varchar(255) not null
 )ENGINE=InnoDB;
 
 -- 
--- Creation de la table Compte
+-- Création de la table Compte
 -- 
 
 create table if not exists comptes(
 id int auto_increment primary key,
+code int not null,
+solde int,
+dateCreation date not null,
+typeCompte varchar(32) not null,
+decouvert int,
+taux float
+)ENGINE=InnoDB;
 
+-- 
+-- Création de la table Employé
+-- 
+
+create table if not exists employes(
+id int auto_increment primary key,
+nom varchar(32) not null,
+prenom varchar(32) not null,
+tel int(25) not null,
+adresse varchar(255) not null,
+email varchar(255),
+login varchar(32) not null,
+mdp varchar(32) not null,
+typeEmploye varchar(32)
+)ENGINE=InnoDB;
+
+-- 
+-- Création de la table Carte 
+-- 
+
+create table if not exists carte(
+id int auto_increment primary key,
+typeCarte varchar(32) not null
 )ENGINE=InnoDB;
