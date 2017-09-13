53112a0ea3608d4dde09aeb59b6c2bcbbb333a8e
Xavier <xpersil@gmail.com>
2017-09-13 10:54:35 +0200

    "Commencement test"

---
+++ proxiBanque/src/test/java/com/huios/proxiBanque/testUnitaire/TestConseiller.java
@@ -0,0 +1,29 @@
+package com.huios.proxiBanque.testUnitaire;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.huios.proxiBanque.metier.Agence;
+import com.huios.proxiBanque.metier.Conseiller;
+
+public class TestConseiller {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCreationAgence() {
+		Agence ag = new Agence();
+		
+	}
+	
+	@Test
+	public void testCreationConseil() {
+		Conseiller c= new Conseiller();
+		
+	}
+
+}
